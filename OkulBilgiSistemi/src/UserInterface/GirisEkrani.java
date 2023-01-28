59c88488c8f4a81a78561d2981644c7e0110bc57
Salih DAMAR <47279445+salihdamar@users.noreply.github.com>
2023-01-28 21:28:51 +0300

    giris button border

--- OkulBilgiSistemi/src/UserInterface/GirisEkrani.java
+++ OkulBilgiSistemi/src/UserInterface/GirisEkrani.java
@@ -298,9 +298,9 @@ public class GirisEkrani extends javax.swing.JFrame {
                 .addComponent(jTextField_KurtarmaKodu, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jLabel_SifremiUnuttum)
-                .addGap(18, 18, 18)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jButton_GirisYap, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(18, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout jPanel_GirisParametreleriLayout = new javax.swing.GroupLayout(jPanel_GirisParametreleri);
